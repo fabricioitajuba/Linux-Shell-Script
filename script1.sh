136fee8744112c26424cd70e48fdbb3a82d0ae98
Fabrício de Lima Ribeiro <31161759+fabricioitajuba@users.noreply.github.com>
2019-03-10 19:52:51 -0300

    Delete script1.sh

--- script1.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#Código de exemplo
-echo "Seu nome de usuário é:"
-whoami
-echo "Info de hora atual e tempo que o computador está ligado:"
-uptime
-echo "O script está executando do diretório:"
-pwd
